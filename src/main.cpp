5a8ca58e06be1b0443e6a48f995f03cea728746d
Pontus Liedgren <pop2strong4u@gmail.com>
2020-10-02 15:08:17 +0200

    Initial DC motor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,76 @@
 #include <Arduino.h>
 
-int blue_led = 13;
-int green_led = 12;
-int red_led = 11;
-int buttonPin = 2;
 
-int buttonState = 0;
+#define MOTOR_EN_1_2  10
+#define MOTOR_IN1     9
+#define MOTOR_IN2     8
+
+#define slow 64
+#define normal 128
+#define fast 255
+
+int Speed; 
+
+void Forward_Rev(void){
+  analogWrite(MOTOR_EN_1_2, Speed);
+  digitalWrite(MOTOR_IN1, HIGH);
+  digitalWrite(MOTOR_IN2, LOW);
+}
+
+void Backward_Rev(void){
+  analogWrite(MOTOR_EN_1_2, Speed);
+  digitalWrite(MOTOR_IN1, LOW);
+  digitalWrite(MOTOR_IN2, HIGH);
+}
+
+void Forward_ramp_up(void){
+  digitalWrite(MOTOR_IN1, HIGH);
+  digitalWrite(MOTOR_IN2, LOW);
+  for (int i=0; i<255; i++) { analogWrite(MOTOR_EN_1_2, i); delay(10); } } void Forward_ramp_down(void){ digitalWrite(MOTOR_IN1, HIGH); digitalWrite(MOTOR_IN2, LOW); for (int i=255; i>=0; i--) {
+    analogWrite(MOTOR_EN_1_2, i);
+    delay(10);
+  }
+}
+
+void Backward_ramp_up(void){
+  digitalWrite(MOTOR_IN1, LOW);
+  digitalWrite(MOTOR_IN2, HIGH);
+  for (int i=0; i<255; i++) { analogWrite(MOTOR_EN_1_2, i); delay(10); } } void Backward_ramp_down(void){ digitalWrite(MOTOR_IN1, LOW); digitalWrite(MOTOR_IN2, HIGH); for (int i=255; i>=0; i--) {
+    analogWrite(MOTOR_EN_1_2, i);
+    delay(10);
+  }
+}
+
+void Brake(void){
+  digitalWrite(MOTOR_IN1, HIGH);
+  digitalWrite(MOTOR_IN2, HIGH);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
-  pinMode(blue_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-  pinMode(red_led, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  Serial.println("L293D DC motor test");
 
+  pinMode(MOTOR_EN_1_2, OUTPUT);
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) {
-    digitalWrite(blue_led, HIGH);
-    delay(1000);
-    digitalWrite(green_led, HIGH);
-    delay(1000);
-  }
-  else {
-    digitalWrite(red_led, HIGH);
-    
-  }
+  Speed=normal; // Normal Speed
+
+  Forward_Rev(); 
+  delay(1000);
+  Brake();
+  delay(500);
+  Backward_Rev(); 
+  delay(1000);
+  Brake();
+  delay(500);
+  Forward_ramp_up();
+  Forward_ramp_down();
+  Backward_ramp_up();
+  Backward_ramp_down();
+  
 }
