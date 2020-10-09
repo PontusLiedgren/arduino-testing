7f0b88c017461353b4501d776a0b9c5ac31ff1b2
Pontus Liedgren <pop2strong4u@gmail.com>
2020-10-09 13:58:38 +0200

    dubbel servos

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,25 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo cool_servo;
+Servo cool_servo, cool_servo2;
 int servo_position = 0;
 
 
 void setup() {
   
   cool_servo.attach (9);
+  cool_servo2.attach (10);
 }
 
 void loop() {
   for (servo_position = 0; servo_position<= 180; servo_position += 1){
     cool_servo.write(servo_position);
-    delay(10);
+    cool_servo2.write(servo_position);
+    delay(15);
   }
   for (servo_position=180; servo_position>= 0; servo_position -=1){
     cool_servo.write(servo_position);
-    delay(10);
+    cool_servo2.write(servo_position);
+    delay(15);
   }
 }
